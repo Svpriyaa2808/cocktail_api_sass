26ae818a9d2896a0a500a54351bf30cfcd560e72
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-07-28 22:20:44 +0200

    commit

--- src/components/WelcomeMessage/index.tsx
+++ src/components/WelcomeMessage/index.tsx
@@ -7,7 +7,7 @@ const WelcomeMessage = () => {
                 <h3 className="intro__content--title">Discover the World of Cocktails</h3>
                 <p className="intro__content--para">Cocktails are a delightful fusion of flavors, aromas, and creativity. From timeless classics
                     like the Mojito and Margarita to modern mixes with exotic twists, there's a cocktail for every taste and occasion.Whether you're a mixology enthusiast or
-                     just looking for a fun new drink, we've got you covered.</p>
+                 just looking for a fun new drink, we've got you covered.</p>
             </div>
 
             <div className="intro__image">
