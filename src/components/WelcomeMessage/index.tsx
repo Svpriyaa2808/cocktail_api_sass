736eb03edcf2c707f0c74d7ff36fd4f26e18da7b
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-07-28 22:20:14 +0200

    adding media query for welcomemessage

--- src/components/WelcomeMessage/index.tsx
+++ src/components/WelcomeMessage/index.tsx
@@ -6,8 +6,8 @@ const WelcomeMessage = () => {
             <div className="intro__content">
                 <h3 className="intro__content--title">Discover the World of Cocktails</h3>
                 <p className="intro__content--para">Cocktails are a delightful fusion of flavors, aromas, and creativity. From timeless classics
-                    like the Mojito and Margarita to modern mixes with exotic twists, there's a cocktail for every taste and occasion.Whether you're a mixology enthusiast or just looking for a fun new drink, we've got you covered.</p>
-                <p> Click the button to get a random cocktail recipe!</p>
+                    like the Mojito and Margarita to modern mixes with exotic twists, there's a cocktail for every taste and occasion.Whether you're a mixology enthusiast or
+                     just looking for a fun new drink, we've got you covered.</p>
             </div>
 
             <div className="intro__image">
